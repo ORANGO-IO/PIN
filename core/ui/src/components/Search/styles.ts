395e82714bdd942b2e1b0087ed4d8e4eccb704ec
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-20 15:14:07 -0300

    feat(component): add Search component

---
+++ core/ui/src/components/Search/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export default styled.div`
+    width:100%;
+    height:auto;
+    display:flex;
+    column-gap:8px;
+    align-items:center;
+    justify-content:center;
+
+    button{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        width: 27px;
+        padding:0;
+        margin:0;
+        height: 27px;
+box-sizing:border-box;
+        background: linear-gradient(180deg, #E0E0E0 0%, #F3F3F3 100%);
+        border: 1px solid #DB815D;
+        box-shadow: 0px 0px 2px #DB815D, 0px 1px 0px rgba(255, 255, 255, 0.85), inset 0px 1px 0px 1px #FFFFFF;
+        border-radius: 5px;
+    }
+`
