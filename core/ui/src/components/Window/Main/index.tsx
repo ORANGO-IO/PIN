c06d61ef81d7985b0ce556bfabab2605d3e3d7d9
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-22 10:08:22 -0300

    feat(component): add component children in main component

--- core/ui/src/components/Window/Main/index.tsx
+++ core/ui/src/components/Window/Main/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Container from './styles'
 
-const Main = () => (
-    <Container></Container>
+const Main:React.FC<{children:React.ReactNode}> = ({ children }) => (
+    <Container>{children}</Container>
 )
 
 export default Main
--- core/ui/src/components/Window/Main/styles.ts
+++ core/ui/src/components/Window/Main/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export default styled.main`
-width:100%;
-height:100%;
-background: #F2F1F0;
+    width:100%;
+    height:100%;
+    background: #F2F1F0;
 `
