068556e44d6d86310696cde9967f97591560b3b5
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-23 10:06:28 -0300

    feat(component): remove jsx of main component and change style.ts name to index.ts

--- core/ui/src/components/Window/Main/index.ts
+++ core/ui/src/components/Window/Main/index.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export default styled.main`
+export default styled.div`
     width:100%;
     height:100%;
     background: #F2F1F0;
--- core/ui/src/components/Window/Main/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Container from './styles'
-
-const Main:React.FC<{children:React.ReactNode}> = ({ children }) => (
-    <Container>{children}</Container>
-)
-
-export default Main
