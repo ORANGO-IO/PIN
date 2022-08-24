78196172b129688f116da14094545e8dc4f9255f
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-24 08:31:46 -0300

    feat(component): add badge component

---
+++ core/ui/src/components/Badge/index.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export default styled.div<{colorText:string;background:string}>`
+    color:${({ colorText }) => colorText};
+    background:${({ background }) => background};
+    border: 1px solid #929292;
+    box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.85), inset 0px 1px 0px 1px rgba(255, 255, 255, 0.2);
+    border-radius: 5px;
+    height:14px;
+    padding:2px 4px;
+    font-size: 8px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    border-radius: 5px;
+    width: fit-content;
+`
