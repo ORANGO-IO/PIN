34b5477d5aba2a5497a8a9fefcafdcc4c6ec2098
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-24 08:48:48 -0300

    feat(component): add onclick in prop progras in bottom menu

--- core/ui/src/components/Window/Menu/index.tsx
+++ core/ui/src/components/Window/Menu/index.tsx
@@ -3,8 +3,18 @@ import MenuButton from './components/Button'
 import ExplicationProgramIcon from './components/ExplicationProgramIcon'
 import ProgramIcon from './components/ProgramIcon'
 import Container from './style'
+/* global HTMLButtonElement */
 
-type program={ ProgramIcon: React.ReactNode, programText: string;ExplicationProgramText:string; TitleExplication: string, ExplicationProgramIcon: React.ReactNode ;key:number}
+type program={
+  ProgramIcon: React.ReactNode,
+  programText: string;
+  ExplicationProgramText:string;
+  TitleExplication: string,
+  ExplicationProgramIcon: React.ReactNode;
+  key:number;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
 interface MenuProps{
   programs?:program[]
 }
@@ -14,7 +24,7 @@ const Menu:React.FC<MenuProps> = ({ programs }) => {
   const [focusProgram, setFocusProgram] = useState<program>()
   return <Container open={open}>
     {programs && programs.map(program => (
-      <ProgramIcon key={program.key} onMouseEnter={() => setFocusProgram(program)} onMouseLeave={() => setFocusProgram(undefined)} text={program.programText} />
+      <ProgramIcon onClick={program.onClick} key={program.key} onMouseEnter={() => setFocusProgram(program)} onMouseLeave={() => setFocusProgram(undefined)} text={program.programText} />
     ))}
     <ExplicationProgramIcon show={!!focusProgram} className='explication' title={focusProgram?.TitleExplication } icon={focusProgram?.ExplicationProgramIcon} text={focusProgram?.ExplicationProgramText} style={{ position: 'absolute', bottom: '104px', width: '80%' }}/>
     <MenuButton onClick={() => setOpen(!open)} className="buttonMenu"/>
