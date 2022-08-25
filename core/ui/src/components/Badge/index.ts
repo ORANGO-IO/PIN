1cc012510f57178f585d89df6b61084689337cdf
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-25 12:07:31 -0300

    feat(component): add default background and colorText in Badge component

--- core/ui/src/components/Badge/index.ts
+++ core/ui/src/components/Badge/index.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
-export default styled.div<{colorText:string;background:string}>`
-    color:${({ colorText }) => colorText};
-    background:${({ background }) => background};
+const DEFAULT_BACKGROUND = 'linear-gradient(180deg, #30CDFF 0%, #0D82A7 100%)'
+const DEFAULT_COLOR_TEXT = '#fff'
+
+export default styled.div<{colorText?:string;background?:string}>`
+    color:${({ colorText }) => colorText || DEFAULT_COLOR_TEXT};
+    background:${({ background }) => background || DEFAULT_BACKGROUND};
     border: 1px solid #929292;
     box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.85), inset 0px 1px 0px 1px rgba(255, 255, 255, 0.2);
     border-radius: 5px;
