abd1f373ddc551d06f15f360d37294ed0e56fcd9
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-27 10:55:47 -0300

    fix(component): add badge optional in card component

--- core/ui/src/components/Card/index.tsx
+++ core/ui/src/components/Card/index.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import Badge from '../Badge'
-import Container from './styles'
+import React from "react";
+import Badge from "../Badge";
+import Container from "./styles";
 
-interface CardProps{
-    subtitleTexts:{id:string, text:string}[];
-    title:string;
-    badges:{id:string;text:string;colorBackground?:string;colorText?:string}[]
-
+interface CardProps {
+  subtitleTexts: { id: string; text: string }[];
+  title: string;
+  badges?: {
+    id: string;
+    text: string;
+    colorBackground?: string;
+    colorText?: string;
+  }[];
 }
 
-const Card:React.FC<CardProps> = ({ subtitleTexts, badges, title }) => (
-    <Container>
-        <p className='title'>
-            {title} {badges.map(badge => <Badge key={badge.id} background={badge.colorBackground} colorText={badge.colorText}>
-              {badge.text}
-            </Badge>)}
-        </p>
-        <div className='subtitle'>
-            {subtitleTexts.map((subtitle) => <p key={subtitle.id}>{subtitle.text}</p>)}
-        </div>
-    </Container>
-)
+const Card: React.FC<CardProps> = ({ subtitleTexts, badges = [], title }) => (
+  <Container>
+    <p className="title">
+      {title}{" "}
+      {badges.map((badge) => (
+        <Badge
+          key={badge.id}
+          background={badge.colorBackground}
+          colorText={badge.colorText}
+        >
+          {badge.text}
+        </Badge>
+      ))}
+    </p>
+    <div className="subtitle">
+      {subtitleTexts.map((subtitle) => (
+        <p key={subtitle.id}>{subtitle.text}</p>
+      ))}
+    </div>
+  </Container>
+);
 
-export default Card
+export default Card;
--- core/ui/src/components/Card/styles.ts
+++ core/ui/src/components/Card/styles.ts
@@ -1,32 +1,31 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export default styled.div`
-    background: #F7F7F7;
-    border-top: 1px solid #E0DEDC;
-    box-shadow: inset 0px 0px 5px rgba(255, 255, 255, 0.5);
-    padding:14px;
-    width:100%;
-    box-sizing:border-box;
-    font-family: 'Ubuntu', sans-serif;
+  background: #f7f7f7;
+  border-top: 1px solid #e0dedc;
+  box-shadow: inset 0px 0px 5px rgba(255, 255, 255, 0.5);
+  padding: 14px;
+  width: 100%;
+  box-sizing: border-box;
+  font-family: "Ubuntu", sans-serif;
 
-    .title{
-        margin-bottom: 3px;
-        font-size: 15px;    
-        margin:0;
-        display:flex;
-        font-family: 'Ubuntu', sans-serif;
+  .title {
+    margin-bottom: 3px;
+    font-size: 15px;
+    margin: 0;
+    display: flex;
+    font-family: "Ubuntu", sans-serif;
+  }
+
+  .subtitle {
+    font-size: 10px;
+    font-weight: 300;
+    color: #3c3c3c;
+    font-size: 10px;
+    color: #3c3c3c;
+    p {
+      margin: 0;
+      font-family: "Ubuntu", sans-serif;
     }
-
-    .subtitle{
-        font-size: 10px;
-        font-weight: 300;
-        color: #3C3C3C;
-        font-size: 10px;
-        color: #3C3C3C;
-        p{
-            margin:0;
-            font-family: 'Ubuntu', sans-serif;
-        }
-
-    }
-`
+  }
+`;
