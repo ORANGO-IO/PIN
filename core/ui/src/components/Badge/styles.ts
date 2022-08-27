24e07d689ae2e788448bea6588f3a498c8364e14
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-27 10:31:34 -0300

    feat(component): fix prettier and refactore Badge component

---
+++ core/ui/src/components/Badge/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const DEFAULT_BACKGROUND = "linear-gradient(180deg, #30CDFF 0%, #0D82A7 100%)";
+const DEFAULT_COLOR_TEXT = "#fff";
+
+export default styled.div<{ colorText?: string; background?: string }>`
+  color: ${({ colorText }) => colorText || DEFAULT_COLOR_TEXT};
+  background: ${({ background }) => background || DEFAULT_BACKGROUND};
+  border: 1px solid #929292;
+  box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.85),
+    inset 0px 1px 0px 1px rgba(255, 255, 255, 0.2);
+  border-radius: 5px;
+  height: 14px;
+  padding: 2px 4px;
+  font-size: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  width: fit-content;
+  font-family: "Ubuntu", sans-serif;
+`;
