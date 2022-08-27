96487d1cb2a1972f11e75a65704f3ffd6f7d1423
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-27 11:12:17 -0300

    fix(hook service): fix error of eslint no-defined

--- core/ui/src/services/hooks/useDimension.ts
+++ core/ui/src/services/hooks/useDimension.ts
@@ -1,7 +1,7 @@
-import { useRef, useState, useEffect, MutableRefObject } from 'react'
-import ResizeObserver from 'resize-observer-polyfill'
+import { useRef, useState, useEffect, MutableRefObject } from "react";
+import ResizeObserver from "resize-observer-polyfill";
 
-const initialState = { width: 0, height: 0 }
+const initialState = { width: 0, height: 0 };
 
 /**
  * const divRef = useRef(null);
@@ -11,24 +11,25 @@ const initialState = { width: 0, height: 0 }
  * @returns
  */
 
-// eslint-disable-next-line no-undef
-const useDimension = (ref:MutableRefObject<HTMLElement | null>) => {
-  const [dimensions, setdDimensions] = useState(initialState)
-  const resizeObserverRef = useRef<ResizeObserver>()
+/* global HTMLElement */
+
+const useDimension = (ref: MutableRefObject<HTMLElement | null>) => {
+  const [dimensions, setdDimensions] = useState(initialState);
+  const resizeObserverRef = useRef<ResizeObserver>();
 
   useEffect(() => {
     resizeObserverRef.current = new ResizeObserver((entries = []) => {
-      entries.forEach(entry => {
-        const { width, height } = entry.contentRect
-        setdDimensions({ width, height })
-      })
-    })
-    if (ref.current) resizeObserverRef.current.observe(ref.current)
+      entries.forEach((entry) => {
+        const { width, height } = entry.contentRect;
+        setdDimensions({ width, height });
+      });
+    });
+    if (ref.current) resizeObserverRef.current.observe(ref.current);
     return () => {
-      if (resizeObserverRef.current) resizeObserverRef.current.disconnect()
-    }
-  }, [ref])
-  return dimensions
-}
+      if (resizeObserverRef.current) resizeObserverRef.current.disconnect();
+    };
+  }, [ref]);
+  return dimensions;
+};
 
-export default useDimension
+export default useDimension;
