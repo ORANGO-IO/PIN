901d6076e14c03d743a8b1580dc9a6caba7a9826
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-29 09:47:29 -0300

    fix(component): move modals to div modals that is outside of root app

--- core/ui/src/components/Window/Modal/index.tsx
+++ core/ui/src/components/Window/Modal/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Container from './styles'
-
+import ReactDOM from 'react-dom'
 interface IModalProps{
     open:boolean;
     modalBackgroundClick?:Function;
     children:React.ReactNode
 }
+const modalRoot = document.getElementById('modals')
 
-const Modal :React.FC<IModalProps> = ({ open, modalBackgroundClick, children }) => (
-    <Container open={open}>
+const Modal :React.FC<IModalProps> = ({ open, modalBackgroundClick, children }) => {
+  const rootElemRef = React.useRef(document.createElement('div'))
+
+  useEffect(() => {
+    if (modalRoot) { modalRoot.appendChild(rootElemRef.current) }
+    return function removeElement () {
+      rootElemRef.current.remove()
+    }
+  }, [])
+
+  return ReactDOM.createPortal(
+    (
+    <Container open={!open}>
         <button onClick={() => {
           if (modalBackgroundClick) {
             modalBackgroundClick()
@@ -16,6 +28,7 @@ const Modal :React.FC<IModalProps> = ({ open, modalBackgroundClick, children }) 
         }} className="button-modal"></button>
         <div>{children}</div>
     </Container>
-)
+    ), rootElemRef.current)
+}
 
 export default Modal
