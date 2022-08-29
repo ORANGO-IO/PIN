07c904837ed1a3e27aa7c4c28aec31ad0d0006b1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-08-29 10:35:53 -0300

    fix(component): remove tags in menu button to image svg

--- core/ui/src/components/Window/Menu/components/Button/index.tsx
+++ core/ui/src/components/Window/Menu/components/Button/index.tsx
@@ -1,24 +1,11 @@
 import React, { ButtonHTMLAttributes } from 'react'
 import Container from './styles'
+import menuIcon from '../../../../../assets/icons/menu.svg'
 /* global HTMLButtonElement */
 
 const MenuButton:React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => (
     <Container type="button" {...props}>
-        <div>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+       <img src={menuIcon} alt="menu"/>
     </Container>
 )
 
